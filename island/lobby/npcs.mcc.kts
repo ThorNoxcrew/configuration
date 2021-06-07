21adf0845d521dc1abefe3f416496e64151eebb0
Jean May <jean.may@noxcrew.com>
2021-06-07 09:40:15 -0500

    Add test script for script loading

---
+++ island/lobby/npcs.mcc.kts
@@ -0,0 +1,6 @@
+mccScript {
+    playerNPC(absoluteVec(235.5, 70.50, -220.5), "Noxite", "Noxite") {
+        roaming(absoluteVec(230.5, 70.0, -210.5), absoluteVec(238.5, 70.0, -226.5))
+        proximityBubbleChat(text("Learn to use asana!"), 5.seconds)
+    }
+}
