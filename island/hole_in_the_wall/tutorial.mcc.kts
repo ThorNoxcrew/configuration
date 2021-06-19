ee4f79e4a440668e4601e6ee6fa85f089efbebff
alexander.soderberg <alexander.soderberg@noxcrew.com>
2021-06-19 03:37:32 +0000

    Feature/island hitw

--- island/hole_in_the_wall/tutorial.mcc.kts
+++ island/hole_in_the_wall/tutorial.mcc.kts
@@ -5,8 +5,6 @@ import com.noxcrew.mcc.commons.server.i18n.sendMessage
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathConfig
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathManager
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
 
 mccScript {
   val cameraPathManager: CameraPathManager by inject()
@@ -16,14 +14,6 @@ mccScript {
   val i18n: I18n by inject()
 
   val playerGroup = (data["players"] as PlayerGroup)
-
-  withContext(Dispatchers.Minecraft) {
-    for (player in playerGroup.onlinePlayers) {
-      player.addPotionEffect(PotionEffect(PotionEffectType.BLINDNESS, 30, 2, false, false, false))
-    }
-  }
-
-  delay(100)
 
   withContext(Dispatchers.Minecraft) {
     for (player in playerGroup.onlinePlayers) {
