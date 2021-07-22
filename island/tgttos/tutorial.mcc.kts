dccf9a421fd5d2bee5ed69c3dcf86f4a548bf698
jean.may <jean.may@noxcrew.com>
2021-07-22 22:25:54 +0000

    Island: Change TGTTOS tutorial

--- island/tgttos/tutorial.mcc.kts
+++ island/tgttos/tutorial.mcc.kts
@@ -1,35 +1,40 @@
+import com.noxcrew.mcc.commons.base.config.get
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
-import com.noxcrew.mcc.commons.server.groups.PlayerGroup
-import com.noxcrew.mcc.commons.server.i18n.sendMessage
+import com.noxcrew.mcc.commons.base.serialization.Serializers
+import com.noxcrew.mcc.commons.server.config.ConfigManager
+import com.noxcrew.mcc.commons.server.module.container.Container
+import com.noxcrew.mcc.commons.server.module.container.ModuleRegistry
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathConfig
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathManager
-import com.noxcrew.mcc.commons.server.sound.playSound
 
 mccScript {
   val cameraPathManager: CameraPathManager by inject()
-  val config = data["cameraPathConfig"] as CameraPathConfig
+  val container = data["container"] as Container
+
+  val configManager = ModuleRegistry.get(ConfigManager::class.java, container)
+  val config = configManager.get(CameraPathConfig::class, Serializers.getHoconDecoder(), container.scopeKeys)
   val cameraPath = CameraPath.fromConfig(config)
 
   val i18n: I18n by inject()
 
-  val playerGroup = (data["players"] as PlayerGroup)
+  val player = data["player"] as Player
 
   // Start camera pathing for the tutorial
   withContext(Dispatchers.Minecraft) {
-    for (player in playerGroup.onlinePlayers) {
+    for (player in player.onlinePlayers) {
       cameraPathManager.start(player, cameraPath)
     }
   }
 
-  playerGroup.playSound("mcc:games.global.timer.grace_period")
+  player.playSound("mcc:games.global.timer.grace_period")
 
   // Send lines 1-6 to the players
   for (line in i18n.range("island.games.tutorial.tgttos.line")) {
-    playerGroup.sendMessage(i18n.translatable(line))
+    player.sendMessage(i18n.translatable(line))
     delay(3000)
   }
   // Send standby text
-  playerGroup.sendMessage(i18n.translatable("island.games.tutorial.standby"))
+  player.sendMessage(i18n.translatable("island.games.tutorial.standby"))
 }
