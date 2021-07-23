e92a107cea2f3a4a60b7a10726b06f2bcfcd7ada
Jean May <jean.may@noxcrew.com>
2021-07-22 19:20:53 -0500

    Island: Update HITW and TGTTOS tutorial script

--- island/hole_in_the_wall/tutorial.mcc.kts
+++ island/hole_in_the_wall/tutorial.mcc.kts
@@ -1,28 +1,35 @@
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
-import com.noxcrew.mcc.commons.server.groups.PlayerGroup
+import com.noxcrew.mcc.commons.server.config.injectModuleConfig
 import com.noxcrew.mcc.commons.server.i18n.sendMessage
+import com.noxcrew.mcc.commons.server.i18n.translateLines
+import com.noxcrew.mcc.commons.server.module.container.Container
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathConfig
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathManager
+import com.noxcrew.mcc.commons.server.sound.playSound
+import org.bukkit.entity.Player
 
 mccScript {
   val cameraPathManager: CameraPathManager by inject()
-  val config = data["cameraPathConfig"] as CameraPathConfig
+  val container = data["container"] as Container
+
+  val config: CameraPathConfig by container.injectModuleConfig()
   val cameraPath = CameraPath.fromConfig(config)
 
   val i18n: I18n by inject()
 
-  val playerGroup = (data["players"] as PlayerGroup)
+  val player = data["player"] as Player
 
   withContext(Dispatchers.Minecraft) {
-    for (player in playerGroup.onlinePlayers) {
-      cameraPathManager.start(player, cameraPath)
-    }
+    cameraPathManager.start(player, cameraPath)
   }
 
-  delay(2000)
-  playerGroup.sendMessage(i18n.translatable("island.games.tutorial.hitw.line1"))
-  delay(2000)
-  playerGroup.sendMessage(i18n.translatable("island.games.tutorial.hitw.line2"))
+  for (line in player.translateLines("island.games.tutorial.hitw")) {
+    player.sendMessage(line)
+    delay(3000)
+  }
+
+  // Send standby text
+  player.sendMessage(i18n.translatable("island.games.tutorial.standby"))
 }
--- island/tgttos/tutorial.mcc.kts
+++ island/tgttos/tutorial.mcc.kts
@@ -1,11 +1,8 @@
-import com.noxcrew.mcc.commons.base.config.get
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
-import com.noxcrew.mcc.commons.base.serialization.Serializers
-import com.noxcrew.mcc.commons.server.config.ConfigManager
-import com.noxcrew.mcc.commons.server.i18n.sendMessage
+import com.noxcrew.mcc.commons.server.config.injectModuleConfig
+import com.noxcrew.mcc.commons.server.i18n.translateLines
 import com.noxcrew.mcc.commons.server.module.container.Container
-import com.noxcrew.mcc.commons.server.module.container.ModuleRegistry
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathConfig
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathManager
@@ -16,8 +13,7 @@ mccScript {
   val cameraPathManager: CameraPathManager by inject()
   val container = data["container"] as Container
 
-  val configManager = ModuleRegistry.get(ConfigManager::class.java, container)
-  val config = configManager.get(CameraPathConfig::class, Serializers.getHoconDecoder(), container.scopeKeys)
+  val config: CameraPathConfig by container.injectModuleConfig()
   val cameraPath = CameraPath.fromConfig(config)
 
   val i18n: I18n by inject()
@@ -32,7 +28,7 @@ mccScript {
   player.playSound("mcc:games.global.timer.grace_period")
 
   // Send lines 1-6 to the players
-  for (line in i18n.range("island.games.tutorial.tgttos.line")) {
+  for (line in player.translateLines("island.games.tutorial.tgttos")) {
     player.sendMessage(i18n.translatable(line))
     delay(3000)
   }
