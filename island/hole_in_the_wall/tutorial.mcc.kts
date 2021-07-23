cf9ce32687a3806ff0e48946e2524813a318c845
alexander.soderberg <alexander.soderberg@noxcrew.com>
2021-07-23 18:49:22 +0000

    Feature/modifiers

--- island/hole_in_the_wall/tutorial.mcc.kts
+++ island/hole_in_the_wall/tutorial.mcc.kts
@@ -4,7 +4,6 @@ import com.noxcrew.mcc.commons.server.config.injectModuleConfig
 import com.noxcrew.mcc.commons.server.game.modifier.ModifierTitles
 import com.noxcrew.mcc.commons.server.i18n.sendMessage
 import com.noxcrew.mcc.commons.server.i18n.translateLines
-import com.noxcrew.mcc.commons.server.module.container
 import com.noxcrew.mcc.commons.server.module.container.Container
 import com.noxcrew.mcc.commons.server.module.container.injectModule
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
