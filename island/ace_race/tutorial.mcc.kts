b391410025e265be1833f953198dfff49a71298e
alexander.soderberg <alexander.soderberg@noxcrew.com>
2021-07-27 23:58:40 +0200

    Ace Race Tutorial

--- island/ace_race/tutorial.mcc.kts
+++ island/ace_race/tutorial.mcc.kts
@@ -1,28 +1,67 @@
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
-import com.noxcrew.mcc.commons.server.groups.PlayerGroup
+import com.noxcrew.mcc.commons.base.text.text
+import com.noxcrew.mcc.commons.server.config.injectModuleConfig
+import com.noxcrew.mcc.commons.server.game.modifier.ModifierTitles
 import com.noxcrew.mcc.commons.server.i18n.sendMessage
+import com.noxcrew.mcc.commons.server.i18n.translateLines
+import com.noxcrew.mcc.commons.server.module.container.Container
+import com.noxcrew.mcc.commons.server.module.container.injectModule
 import com.noxcrew.mcc.commons.server.path.camera.CameraPath
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathConfig
 import com.noxcrew.mcc.commons.server.path.camera.CameraPathManager
+import com.noxcrew.mcc.commons.server.text.font.CustomGlyphProvider
+import java.time.Duration
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.title.Title
+import org.bukkit.entity.Player
 
 mccScript {
   val cameraPathManager: CameraPathManager by inject()
-  val config = data["cameraPathConfig"] as CameraPathConfig
+  val container = data["container"] as Container
+
+  val config: CameraPathConfig by container.injectModuleConfig()
   val cameraPath = CameraPath.fromConfig(config)
+  val modifierTitles: ModifierTitles by container.injectModule()
 
   val i18n: I18n by inject()
 
-  val playerGroup = (data["players"] as PlayerGroup)
+  val player = data["player"] as Player
+
+  val customGlyphProvider: CustomGlyphProvider by player.injectModule()
+
+  player.showTitle(
+      Title.title(
+          customGlyphProvider["mcc:gui.black_box_1"].toComponent(NamedTextColor.WHITE),
+          Component.empty(),
+          Title.Times.of(Duration.ZERO, Duration.ofSeconds(1), Duration.ofSeconds(1))))
 
   withContext(Dispatchers.Minecraft) {
-    for (player in playerGroup.onlinePlayers) {
-      cameraPathManager.start(player, cameraPath)
-    }
+    cameraPathManager.start(player, cameraPath)
+
+    delay(1000)
+    player.showTitle(
+        Title.title(
+            text("Ace Race") {
+              style {
+                color(NamedTextColor.GOLD)
+                decorate(TextDecoration.BOLD)
+              }
+            },
+            Component.empty(),
+            Title.Times.of(Duration.ofSeconds(1), Duration.ofSeconds(1), Duration.ofSeconds(1))))
   }
 
-  delay(2000)
-  playerGroup.sendMessage(i18n.translatable("island.games.tutorial.hitw.line1"))
-  delay(2000)
-  playerGroup.sendMessage(i18n.translatable("island.games.tutorial.hitw.line2"))
+  delay(3000)
+  withContext(Dispatchers.Minecraft) { modifierTitles.showToPlayer(player) }
+
+  for (line in player.translateLines("island.games.tutorial.ace_race")) {
+    player.sendMessage(line)
+    delay(3000)
+  }
+
+  // Send standby text
+  player.sendMessage(i18n.translatable("island.games.tutorial.standby"))
 }
