c03ed0e253f5da2ebcd14728dae22038104272ae
Davud <davud.kosovac@noxcrew.com>
2021-07-31 14:25:04 +0200

    Deleted test NPCs, modified vin petrol

---
+++ island/lobby/npc_vin_petrol.kts
@@ -0,0 +1,34 @@
+import com.noxcrew.mcc.commons.base.i18n.I18n
+import com.noxcrew.mcc.commons.base.inject
+import java.util.LinkedList
+
+mccScript {
+  val i18n: I18n by inject()
+
+  // Below is a Player NPC with the texture and texture signature provided, you can get the values
+  // by uploading a skin to https://mineskin.org/ doing it this way ensures we don't reach mojang's
+  // API limit :)
+  playerNPC(
+      // The spawn position of the NPC, must use decimals
+      absoluteVec(395.5, 34.0, 587.5),
+      // Translation key for name or untranslatable text
+      "<glyph=mcc:icon.emojis.scream> Vin Petrol",
+      // The texture provided from https://mineskin.org/
+      "ewogICJ0aW1lc3RhbXAiIDogMTYyMzg3MjIxMDU0OCwKICAicHJvZmlsZUlkIiA6ICJjZGM5MzQ0NDAzODM0ZDdkYmRmOWUyMmVjZmM5MzBiZiIsCiAgInByb2ZpbGVOYW1lIiA6ICJSYXdMb2JzdGVycyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9kNTI5ZjA3OWRjYTVmMGE0NDU4MzFhMjBhNjg4OTMzMDEyMzkzMjdkMzU3OTY1MTViMjYxNjM2YTZhODYxMWQ3IgogICAgfQogIH0KfQ==",
+      // The texture signature provided from https://mineskin.org/
+      "DGnx2+KiKtyiveQHhixg38hFtTbe5VOxA/1hcDnUKJxPnpkXXXx91W/NaF8owi8v/3srXNFnN2TNV2pwbEoA4kMFy7MZY2TP2Ah3MFieahjX8dHe0D/8rQjFLoYYAQTe9VNSDuoPrwOuPZ7IEwmYyITzxihWvvj5uA4zR4FR11Yw7XD0ZEAyk9Yi+TBE2EfbTuS6Dvru95I4Z8LdUhSzueH41X+6+Tdvs3bPcc3MarztuLHEVQpLewGG58YsY4rA4MEcChKdhLIlF5Wd7JDQKsMYRC1PGQDyFPJjAu78h3/nQ6tvgD6CXkG56Aaui0wmpUJFllrGeKaxYuRgi5V9MbC6cCQBASTgB3MMfCVTz/Z5SpPNa0Km4kMkPnHvnTIyqX2dxONPbphM1dg5BGTgr9sq40dIC9OyIA8I6danFuoIQhhRHf+o/icpwSr4M3EfBeUJoy4nTqqnHKWUipcyw9Ary0rL53vqdbEoNaKmC1/O36QmQ6VcaLcU7KOI0kHUYF6+BigK3cJnRKtOjJoLL/B/pao1sQDo0gmFo7l6Kt3ltmzWWrPDkGQI4fu8NW4sRYGPB7DlTnfSsNGh6QK5dck2hUDc8/+pzIr8y28VGOec7gJROtWoh1idci6B4N7/195uXl6tM8+RHntJ6hBdkQV1Ztq/SwHdfx8V0tz8Fao=") {
+    // Set the direction the NPC is facing when spawned, use decimals for this
+    rotation(-180.0, -4.0)
+
+
+    // Bubble chat, randomly selecting one from the list every 4 seconds
+    proximityBubbleChat(
+        listOf(
+            i18n.translatable("island.npc.jacklumberdo.dialogue1"),
+            i18n.translatable("island.npc.jacklumberdo.dialogue2"),
+            i18n.translatable("island.npc.jacklumberdo.dialogue3")),
+        45.seconds)
+
+    type("ambient")
+  }
+}
