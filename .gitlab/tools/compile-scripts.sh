ca331818825fb599f50f0ce95acccc508bd7fa58
Michael Hoekema <michael.hoekema@noxcrew.com>
2021-08-02 17:32:23 +0200

    Force overwriting zstd files

--- .gitlab/tools/compile-scripts.sh
+++ .gitlab/tools/compile-scripts.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 compile_script() {
 	java -jar /mcc-script-cli.jar $1 compiled/$1
-        zstd compiled/$1/*.bytecode
+        zstd -f compiled/$1/*.bytecode
 }
 
 SCRIPT_DIRECTORIES=$(find . -type f -name "*.mcc.kts" -printf "%h\n" | cut -c 3- | uniq)
