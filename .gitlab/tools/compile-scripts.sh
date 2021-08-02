c92e6496cdff7f94c31a97a05f7eafcc27ade390
Michael Hoekema <michael.hoekema@noxcrew.com>
2021-08-02 17:13:09 +0200

    Fix duplicate string removal

--- .gitlab/tools/compile-scripts.sh
+++ .gitlab/tools/compile-scripts.sh
@@ -5,7 +5,7 @@ compile_script() {
 	java -jar /mcc-script-cli.jar $1 compiled/$1
 }
 
-SCRIPT_DIRECTORIES=$(find . -type f -name "*.mcc.kts" -printf "%h\n" | cut -c 3- | uniq -u)
+SCRIPT_DIRECTORIES=$(find . -type f -name "*.mcc.kts" -printf "%h\n" | cut -c 3- | uniq)
 
 for i in $SCRIPT_DIRECTORIES; do
 	compile_script $i
