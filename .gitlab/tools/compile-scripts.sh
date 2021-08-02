639a7f64c535eabd0d6b6d31191e51ca0478ef68
Michael Hoekema <michael.hoekema@noxcrew.com>
2021-08-02 17:29:45 +0200

    Don't remove the original bytecode file for caching

--- .gitlab/tools/compile-scripts.sh
+++ .gitlab/tools/compile-scripts.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 compile_script() {
 	java -jar /mcc-script-cli.jar $1 compiled/$1
         zstd compiled/$1/*.bytecode
-        rm compiled/$1/*.bytecode
 }
 
 SCRIPT_DIRECTORIES=$(find . -type f -name "*.mcc.kts" -printf "%h\n" | cut -c 3- | uniq)
