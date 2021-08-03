043b517eecd13b95d35f92ecfe8302eb2e832a3b
Michael Hoekema <michael.hoekema@noxcrew.com>
2021-08-03 02:43:39 +0200

    Add csv compression

---
+++ .gitlab/tools/compress-csv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CSV_DIRECTORIES=$(find . -type f -name "*.csv" -printf "%h\n" | cut -c 3- | uniq)
+
+for i in $CSV_DIRECTORIES; do
+	zstd -f $i/*.csv
+        rm *.csv
+done
