b387bf14db5eaa9e4d99cfeaa57efe243d00da6c
alexander.soderberg <alexander.soderberg@noxcrew.com>
2021-08-11 22:23:04 +0200

    add shop

--- island/lobby/npc_pretty_uppy_lady.mcc.kts
+++ island/lobby/npc_pretty_uppy_lady.mcc.kts
@@ -1,8 +1,12 @@
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
+import com.noxcrew.mcc.island.base.shop.ShopManager
+import com.noxcrew.mcc.island.server.shop.ShopGui
+import kotlin.time.seconds
 
 mccScript {
   val i18n: I18n by inject()
+  val shopManager: ShopManager by inject()
 
   playerNPC(
       absoluteVec(366.5, 34.0, 494.5),
@@ -11,7 +15,14 @@ mccScript {
       "vMZWO1ZgmDgLk1+aA0m4eWvh/kdpG6snyrYDB5K1QUc9bW3fA5rb+iha6DRrq1tEBlhEID+X8F1mWjNCL19Cwbsmy1371zVj1dIyD0VDTE3FzCUiScFEFXe0HgNyxse5/A3Dh6Q1J3poQ7IPdWm2zDr9cNxYqdIHeu2uK7YOt2jEt9aUyL8FyOj7Hzw8t1gmPXRWhQtIQh4ViJ48/szvbWbDmvE9qK22N4taAfU1RCG9BPBe1DLp/wA2qQ6OGM6tNdK8yPTkSiTb7wYMRS3xwum8VmTZjS9R5NRW+FBYkcCW4PZjSd7Mr4qeF+au996BiwZUI6iPAgnoYDcIWChDG4TMH06Y6WOI3Jaoc/Z3BTPKOpAPB2ybEyHo3kj7Nf3hcYX5+u6QJjwYn3NNYyO2vh0rxEG9s2wxn+W5u+l/xVfuVPbbzd5RahGRvIrlKPrWdIPMbIFqx/A1aWmSH/9EyHPv1tMqavaMdiBM5aoOZ8VpIDpmXxzFgw7CiXCt43NnIJtsIaJhpuJIbnhk224P9ArBgUIXKrc48nNkyCpm+tOqK0WrduZYq7kM1A300HSSuQrQLBox3Jegoa98Irxt/2DclhIEza2ZDRmETGHvgXSrsRVEPSLKq+s/S1XTHkXg4Efo4wVsvhFGa4oqbAfQ4QgAbZBkcgNhk/i3ipDXmHg=") {
     rotation(75.0, 0.0)
     lookClosePerPlayer(6)
-	
-	type("shop")
+    cooldown(1.seconds)
+
+    interactBehavior { npc, player ->
+      withContext(Dispatchers.Minecraft) {
+        ShopGui(currencyType, "particleStore", player).open(player)
+      }
+    }
+
+    type("shop")
   }
 }
