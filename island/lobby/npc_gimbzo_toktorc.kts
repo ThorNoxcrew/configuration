729088c1a279f3960187ea0535643df41c0eb9dc
Adam Ledet <adamcledet@gmail.com>
2021-08-16 10:05:07 -0500

    Added Stores
    Turned Springsteen into Music Store NPC.
    Added Gimbzo (Gadget Store) NPC. Uses Uppy Lady model.
    Fixed turntable item.

---
+++ island/lobby/npc_gimbzo_toktorc.kts
@@ -0,0 +1,29 @@
+import com.noxcrew.mcc.commons.base.currency.CurrencyType
+import com.noxcrew.mcc.commons.base.i18n.I18n
+import com.noxcrew.mcc.commons.base.inject
+import com.noxcrew.mcc.island.base.shop.ShopManager
+import com.noxcrew.mcc.island.server.shop.ShopGui
+import kotlin.time.seconds
+
+mccScript {
+  val i18n: I18n by inject()
+  val shopManager: ShopManager by inject()
+
+  playerNPC(
+      absoluteVec(362, 34.0, 478),
+      "Gimbzo TokTorc",
+      "ewogICJ0aW1lc3RhbXAiIDogMTYyODYyODg5ODAzNCwKICAicHJvZmlsZUlkIiA6ICI5MzJlN2QxNGI1YmU0NjQyOTIwYWQ5MDEyZGExMTQ0MSIsCiAgInByb2ZpbGVOYW1lIiA6ICJqaW1teTIwMTgwMTMwIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzJmZDlhZmY3ZDA5MGFmZjdiMjhlNTBjMjlhZjhkNmQzNjJkMzRlZDM3MjU2ZjcxNjI3ODA5MzM3NTI4Mjg2OCIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9",
+      "vMZWO1ZgmDgLk1+aA0m4eWvh/kdpG6snyrYDB5K1QUc9bW3fA5rb+iha6DRrq1tEBlhEID+X8F1mWjNCL19Cwbsmy1371zVj1dIyD0VDTE3FzCUiScFEFXe0HgNyxse5/A3Dh6Q1J3poQ7IPdWm2zDr9cNxYqdIHeu2uK7YOt2jEt9aUyL8FyOj7Hzw8t1gmPXRWhQtIQh4ViJ48/szvbWbDmvE9qK22N4taAfU1RCG9BPBe1DLp/wA2qQ6OGM6tNdK8yPTkSiTb7wYMRS3xwum8VmTZjS9R5NRW+FBYkcCW4PZjSd7Mr4qeF+au996BiwZUI6iPAgnoYDcIWChDG4TMH06Y6WOI3Jaoc/Z3BTPKOpAPB2ybEyHo3kj7Nf3hcYX5+u6QJjwYn3NNYyO2vh0rxEG9s2wxn+W5u+l/xVfuVPbbzd5RahGRvIrlKPrWdIPMbIFqx/A1aWmSH/9EyHPv1tMqavaMdiBM5aoOZ8VpIDpmXxzFgw7CiXCt43NnIJtsIaJhpuJIbnhk224P9ArBgUIXKrc48nNkyCpm+tOqK0WrduZYq7kM1A300HSSuQrQLBox3Jegoa98Irxt/2DclhIEza2ZDRmETGHvgXSrsRVEPSLKq+s/S1XTHkXg4Efo4wVsvhFGa4oqbAfQ4QgAbZBkcgNhk/i3ipDXmHg=") {
+    rotation(75.0, 0.0)
+    lookClosePerPlayer(6)
+    cooldown(1.seconds)
+
+    interactBehavior { npc, player ->
+      withContext(Dispatchers.Minecraft) {
+        ShopGui(CurrencyType.NON_PREMIUM, "gadgetStore", player).open(player)
+      }
+    }
+
+    type("shop")
+  }
+}
--- island/lobby/npc_springsteen.mcc.kts
+++ island/lobby/npc_springsteen.mcc.kts
@@ -1,8 +1,13 @@
+import com.noxcrew.mcc.commons.base.currency.CurrencyType
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
+import com.noxcrew.mcc.island.base.shop.ShopManager
+import com.noxcrew.mcc.island.server.shop.ShopGui
+import kotlin.time.seconds
 
 mccScript {
   val i18n: I18n by inject()
+  val shopManager: ShopManager by inject()
 
   playerNPC(
       absoluteVec(360.5, 32.0, 457.5),
@@ -11,6 +16,13 @@ mccScript {
       "bvTGICxy3xQjnJSDZaD9veK4Ucet6z5sqWuSWN9bmfxPSXj3Oxy4OGgmyYrDTcvRUe9Y74uw7X4tFy8EV/i7PQ1FAwHN1KxWu2EbIzDmfeNdxpuBZGkGVPKeev43n1hIG8862Ii8z/ZLZSZWu8ItD7ZtbkoqhclE3pLsLYcldhNtiMDBd1/Z9bJJrmrQBM+LJJWYCnEmGoy3JldJoxdq0P3vh2Z9c+fU6HiyKgwSKZPSbQcM7/X3IyNd6crdYceO7uinHQ4CI9spBppMse2OMH5BAf+aUGjGXqH8nJNrEEPiDv9gtutRQLJq9b6SrpLlD5o4ERKdj43I6GwacwCY1Phnm43KI0GkKJCUR4zkbqPMu1zHvNciO9CYF6VvZcBJZHJfDq15u2y50OGkEdzRvqfPBynAIg5vpOa64/caWzMu3sZ+atEREJpYR5pICoDV09ml70p7GmMhN/yhTIupjD4vSMhQjrV6rMYR13vP2GXGYxCOu9Bzl0b7SsTVPMdNRRtK5pfwa5u80Fg68W1f58JvCpqSGyvVbb1icFcGvudDDC5N1DydMvtenQmHsQCSFi3O+LiaW57zjd6oXUE/M+Unt1taavkJN4UkOkhvR21kwKVLCP0cbyVoM+Zguxv2s4wwfHJpcdgW1QjSp4KyaGz0cY6S4/xXJK5JuzZ3BSs=") {
     rotation(180.0, 0.0)
     lookClosePerPlayer(6)
+    cooldown(1.seconds)
+    
+    interactBehavior { npc, player ->
+      withContext(Dispatchers.Minecraft) {
+        ShopGui(CurrencyType.NON_PREMIUM, "musicStore", player).open(player)
+      }
+    }
     
     proximityBubbleChat(
         listOf(
