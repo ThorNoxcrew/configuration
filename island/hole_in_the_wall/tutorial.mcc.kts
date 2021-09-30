9f9d5ef791c94185685f5c406c00816c608ce7d6
alexander.soderberg <alexander.soderberg@noxcrew.com>
2021-09-30 16:57:18 +0200

    Tutorials

--- island/hole_in_the_wall/tutorial.mcc.kts
+++ island/hole_in_the_wall/tutorial.mcc.kts
@@ -1,7 +1,6 @@
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
 import com.noxcrew.mcc.commons.base.text.text
-import com.noxcrew.mcc.commons.server.game.modifier.ModifierTitles
 import com.noxcrew.mcc.commons.server.i18n.sendMessage
 import com.noxcrew.mcc.commons.server.i18n.translateLines
 import com.noxcrew.mcc.commons.server.module.container.Container
@@ -17,7 +16,6 @@ import java.time.Duration
 
 mccScript {
     val container = data["container"] as Container
-    val modifierTitles: ModifierTitles by container.injectModule()
 
     val i18n: I18n by inject()
 
@@ -57,7 +55,6 @@ mccScript {
 
     for (line in player.translateLines("island.games.tutorial.hitw")) {
         player.sendMessage(line)
-        delay(3000)
     }
 
     // Send standby text
