3d27f95f17b87a465ba3c11c5db535ef3b5f0f1b
Adam Ledet <adamcledet@gmail.com>
2021-11-18 11:56:36 -0600

    ktlint Fix

--- island/lobby/npc_gimbzo_toktorc.mcc.kts
+++ island/lobby/npc_gimbzo_toktorc.mcc.kts
@@ -1,8 +1,8 @@
-import com.noxcrew.mcc.commons.base.currency.CurrencyType
+// import com.noxcrew.mcc.commons.base.currency.CurrencyType
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
 import com.noxcrew.mcc.island.base.shop.ShopManager
-import com.noxcrew.mcc.island.server.shop.ShopGui
+// import com.noxcrew.mcc.island.server.shop.ShopGui
 import kotlin.time.seconds
 
 mccScript {
@@ -26,13 +26,13 @@ mccScript {
             ),
             30.seconds
         )
-/* 
-        interactBehavior { npc, player ->
+
+        /*interactBehavior { npc, player ->
             withContext(Dispatchers.Minecraft) {
                 ShopGui(CurrencyType.NON_PREMIUM, "gadgetStore", player).open(player)
             }
-        }
- */
+        }*/
+        
         helmet("mcc:island_cosmetics.hair.professor")
         offHand("mcc:island_cosmetics.accessory.pipe_wrench")
 
--- island/lobby/npc_mia_wattington.mcc.kts
+++ island/lobby/npc_mia_wattington.mcc.kts
@@ -1,8 +1,8 @@
-import com.noxcrew.mcc.commons.base.currency.CurrencyType
+// import com.noxcrew.mcc.commons.base.currency.CurrencyType
 import com.noxcrew.mcc.commons.base.i18n.I18n
 import com.noxcrew.mcc.commons.base.inject
 import com.noxcrew.mcc.island.base.shop.ShopManager
-import com.noxcrew.mcc.island.server.shop.ShopGui
+// import com.noxcrew.mcc.island.server.shop.ShopGui
 import kotlin.time.seconds
 
 mccScript {
@@ -18,13 +18,13 @@ mccScript {
         rotation(-90.0, 45.0)
         lookClosePerPlayer(4)
         cooldown(1.seconds)
-/* 
-        interactBehavior { npc, player ->
+        
+        /*interactBehavior { npc, player ->
             withContext(Dispatchers.Minecraft) {
                 ShopGui(CurrencyType.NON_PREMIUM, "gadgetStore", player).open(player)
             }
-        }
- */
+        }*/
+        
         belowName("island.npc.shop.gadgets")
         type("shop")
     }
