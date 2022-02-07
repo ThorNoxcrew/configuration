960e9c58d28396c79807ad3dc1af10c9a92271e2
Michael H <michael.hoekema@noxcrew.com>
2022-02-07 18:50:11 +0000

    script-cli

--- .gitlab/tools/compile-scripts.sh
+++ .gitlab/tools/compile-scripts.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 compile_script() {
-	java -Dkotlin.environment.keepalive=true -jar /mcc-script-cli.jar $1 compiled/$1
+	java -Dkotlin.environment.keepalive=true -jar /script-cli.jar $1 compiled/$1
 }
 
 SCRIPT_DIRECTORIES=$(find . -type f -name "*.mcc.kts" -printf "%h\n" | cut -c 3- | uniq)
